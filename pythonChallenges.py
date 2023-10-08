9b724a9f0eccb12236fa40a6902cc1f7b9e42bd0
Lawrence Alderton <laald2@morgan.edu>
2023-10-08 09:15:47 -0400

    additional python challenge

--- pythonChallenges.py
+++ pythonChallenges.py
@@ -29,4 +29,30 @@ def mid(string):
     if midIndex.is_integer():
         return ""
     else:
-        return string[int(midIndex)] 
+        return string[int(midIndex)]
+
+ '''
+ Online status
+The aim of this challenge is, given a dictionary of people's online status, to count the number of people who are online.
+
+For example, consider the following dictionary:
+
+statuses = {
+    "Alice": "online",
+    "Bob": "offline",
+    "Eve": "online",
+}
+In this case, the number of people online is 2.
+
+Write a function named online_count that takes one parameter. 
+The parameter is a dictionary that maps from strings of names to the string "online" or "offline", as seen above.
+
+Your function should return the number of people who are online.
+ '''
+
+def online_count(dict):
+    onlineNum = 0
+    for key in dict:
+        if dict[key] is 'online':
+            onlineNum += 1
+    return onlineNum
