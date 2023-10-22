6742f6bd25d41cbeb04624cfdb9eb868805a33ad
Lawrence Alderton <laald2@morgan.edu>
2023-10-22 18:25:20 -0400

    include was untracked

---
+++ include/Test/Practice.h
@@ -0,0 +1,16 @@
+#ifndef PRACTICE_H
+#define PRACTICE_H
+#include <iostream>
+#include <string>
+using namespace std;
+namespace Test
+{
+    class Practice
+    {
+        public:
+            Practice();
+            ~Practice();
+            void print(std::string str);
+    };
+}// Test
+#endif //PRACTICE_H
